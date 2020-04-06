9d28ba9b33acc2b88d30323f3ae72f097209c35b
MischaTobias <mischa.tobias4b@gmail.com>
2020-04-05 20:40:39 -0600

    Priority queue started and Tasks class created

---
+++ Models/TasksModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab5_ED1.Models
+{
+    public class TasksModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Proyect { get; set; }
+        public int Priority { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
