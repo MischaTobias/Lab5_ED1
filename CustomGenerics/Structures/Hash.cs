5bfaea77c407bb54d0345fcd1ad82e20e53af773
JORGE WALDEMAR GARCIA BALDIZON <jorgeg.baldizon@gmail.com>
2020-04-16 08:32:02 -0600

    Icomparable to Hash

--- CustomGenerics/Structures/Hash.cs
+++ CustomGenerics/Structures/Hash.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace CustomGenerics.Structures
 {
-    public class Hash<T>
+    public class Hash<T> where T : IComparable
     {
         HashNode<T>[] TablaHash = new HashNode<T>[12];
 
@@ -15,7 +15,7 @@ namespace CustomGenerics.Structures
         {
             HashNode<T> T1 = new HashNode<T>();
             T1.value = InsertV;
-           int code = T1.GetHashCode()%12;
+            int code = T1.GetHashCode()%12;
             if(TablaHash[code] != null)
             {
                 HashNode<T> Aux = TablaHash[12];
@@ -31,41 +31,42 @@ namespace CustomGenerics.Structures
                 TablaHash[code] = T1;
             }
         }
-        //public HashNode<T> Search(string Searchedname)
-        //{
-        //    int code = Searchedname.GetHashCode() % 12;
-        //    //if(TablaHash[code].Name != Searchedname)
-        //    //{
-        //    //    HashNode<T> Aux = TablaHash[code];
-        //    //    while(Aux.Name != Searchedname || Aux.Next !=null)
-        //    //    {
-        //    //        Aux = Aux.Next;
-        //    //    }
-        //    //    if(Aux.Next == null)
-        //    //    {
-        //    //        return null;
-        //    //    }
-        //    //    else
-        //    //    {
-        //    //        return Aux;
-        //    //    }
-        //    //}
-        //    //else
-        //    //{
-        //    //    return TablaHash[code];
-        //    //}
-        //}
-       //public void Remove(string searchedName)
-       //{
-       //     HashNode<T> TaskTR = Search(searchedName);
-       //     if (TaskTR.Next != null)
-       //     {
-       //         TaskTR.Next.Previous = TaskTR.Previous;
-       //     }
-       //     if(TaskTR.Previous!= null)
-       //     {
-       //         TaskTR.Previous.Next = TaskTR.Next;
-       //     }
-       //}   
+        public HashNode<T> Search(T Searchedname)
+        {
+            var SearchedNode = new HashNode<T> { value = Searchedname };
+            int code = SearchedNode.GetHashCode() % 12;
+            if (TablaHash[code].value.CompareTo(Searchedname) != 0)
+            {
+                HashNode<T> Aux = TablaHash[code];
+                while (Aux.value.CompareTo(Searchedname) != 0 && Aux.Next != null)
+                {
+                    Aux = Aux.Next;
+                }
+                if (Aux.Next == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    return Aux;
+                }
+            }
+            else
+            {
+                return TablaHash[code];
+            }
+        }
+        public void Remove(string searchedName)
+        {
+            HashNode<T> TaskTR = Search(searchedName);
+            if (TaskTR.Next != null)
+            {
+                TaskTR.Next.Previous = TaskTR.Previous;
+            }
+            if (TaskTR.Previous != null)
+            {
+                TaskTR.Previous.Next = TaskTR.Next;
+            }
+        }
     }
 }
