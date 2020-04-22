11f9e5070d2abafcc16dc4c4ceb7d3adc8c2cd1e
MischaTobias <mischa.tobias4b@gmail.com>
2020-04-22 08:28:30 -0600

    Assigned view for each user

--- CustomGenerics/Structures/PriorityQueue.cs
+++ CustomGenerics/Structures/PriorityQueue.cs
@@ -169,7 +169,7 @@ namespace CustomGenerics
             node.Father.Key = Key1;
         }
         
-        private Node<T> Delete(Node<T> current, int number)
+        public  Node<T> Delete(Node<T> current, int number)
         {
             Node<T> LastNode = SearchLastNode(Root, 1);
             Node<T> FirstNode = Root;
